f179b65fe45362563a01c9eb0f1524cb6072216f
Mike Litoris <Mike Litoris@172.17.112.61>
2016-05-26 14:46:37 +0200

    asd

---
+++ src/main/java/com/journaldev/struts2/actions/LoginAction.java
@@ -0,0 +1,46 @@
+package com.journaldev.struts2.actions;
+
+import java.util.Map;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.journaldev.struts2.models.User;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+@Results({
+	@Result(name="success", location="/welcome.jsp"),
+	@Result(name="input", location="/login.jsp")
+})
+public class LoginAction extends ActionSupport implements SessionAware, ModelDriven<User>{
+
+	private static final long serialVersionUID = -3369875299120377549L;
+
+	@Action(value="login")
+	public String execute(){
+		System.out.println("inside execute");
+		if("pankaj".equals(user.getUser()) && "admin".equals(user.getPassword())){
+			user.setUserName("Pankaj Kumar");
+			sessionAttributes.put("USER", user);
+			return SUCCESS;
+		}
+		return INPUT;
+	}
+	
+	private User user = new User();
+	private Map<String, Object> sessionAttributes = null;
+
+	@Override
+	public void setSession(Map<String, Object> sessionAttributes) {
+		this.sessionAttributes = sessionAttributes;
+	}
+	
+	@Override
+	public User getModel() {
+		return user;
+	}
+	
+}
